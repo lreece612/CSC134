73763b16fd8f9913a28a2e25511e82a3d16f4615
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-09-08 17:27:52 -0400

    re-adding chapter 1 assignments

---
+++ ch1/Lab1/firstprog.cpp
@@ -0,0 +1,15 @@
+// This is the first program that just writes out a simple message
+//
+// Walter Vaughan
+
+#include <iostream>          // needed to perform C++ I/O
+#include <cstdlib>
+using namespace std;
+
+int main ()
+{
+	cout << "Now is the time for all good men" << endl;
+	cout << "To come to the aid of their party" << endl;
+
+	return 0;
+}
---
+++ ch1/Lab1/semiprob.cpp
@@ -0,0 +1,23 @@
+// This program demonstrates a compile error.
+//
+// Walter Vaughan
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int number;
+	float total;
+	
+	
+	cout << "Today is a great day for Lab";
+	cout << endl << "Let's start off by typing a number of your choice" << endl;
+	cin  >> number;
+	
+	total = number * 2;
+	cout << total << " is twice the number you typed" << endl;
+	
+	return 0;	
+}
