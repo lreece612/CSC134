d9e06a125bbd6de52dd2a374d5dac660fcc0ce1d
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-09-09 19:14:43 -0400

    used experiments/main.cpp to answer some misc. stuff quickly for a question.

--- experiments/main.cpp
+++ experiments/main.cpp
@@ -1,1 +1,10 @@
-int main(){return 0;}
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cout << setw(9) << setprecision(2) << 34.789 << endl;
+	cout << setw(5) << fixed << showpoint << setprecision(3) << 7.0 << endl;
+	cout << fixed << 5.789e+12 << endl;
+	cout << left << setw(7) << 67 << endl;
+}
