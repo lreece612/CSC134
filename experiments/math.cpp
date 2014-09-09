7c692a39e982d828e0b3115255953839016eb2e2
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-09-09 14:12:04 -0400

    created math.cpp to solve a quick expression, for Lab 3, prelab question 1

---
+++ experiments/math.cpp
@@ -0,0 +1,7 @@
+// this is to answer the first question for Lab 3 pre-lab
+#include<iostream>
+using namespace std;
+int main() {
+	cout << (5 - 16 / 2 * 3 + (3 + 2 / 2) - 5) << endl;
+	return 0;
+}
