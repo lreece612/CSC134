8a9402b47ea25449b4bed45367c63bfa275d2f8e
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-09-18 18:48:52 -0400

    used experiment main.cpp to try and declare a function in a stupid way (it didn't work)

--- experiments/main.cpp
+++ experiments/main.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
 int main() {
-	cout << setw(9) << setprecision(2) << 34.789 << endl;
-	cout << setw(5) << fixed << showpoint << setprecision(3) << 7.0 << endl;
-	cout << fixed << 5.789e+12 << endl;
-	cout << left << setw(7) << 67 << endl;
+	return 0;
 }
+
+void shortfunction() = return;
