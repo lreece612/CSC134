004f3b55bc48228e6b27edef46fdb41f4b0bddc7
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-11-25 19:08:19 -0500

    finished ch7 programming challenge 6...meant to do this way earlier.

--- ch07/ProgrammingChallenges/6.analysis.cpp
+++ ch07/ProgrammingChallenges/6.analysis.cpp
@@ -15,7 +15,54 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <limits>
 
 int main() {
+    std::string  filename = "INVALID_INPUT_FILE";
+    std::fstream fs; // main file reader
+
+    // statistical variables, initialized to defaults guaranteed to change
+    // or still be correct
+    double low = std::numeric_limits<double>::max();
+    double high = std::numeric_limits<double>::min();
+    double total = 0;
+    unsigned number_cnt;
+
+    std::cout << "Enter the name of the file to be analyzed: ";
+    std::cin >> filename;
+
+    fs.open(filename.c_str(), std::fstream::in);
+
+    if (!fs.good()) {
+        std::cerr << "\n** Error encountered opening \'" << filename
+                  << "\' **\n";
+        return 1;
+    }
+
+    std::cout << "\nReading \'" << filename << "\'...";
+
+    double number = 0; // individual datum read from file
+    for (number_cnt = 0; fs.good(); number_cnt++ ) {
+        fs >> number;
+        if (number < low)  low  = number;
+        if (number > high) high = number;
+        total += number;
+    }
+
+    if (fs.bad()) {
+        std::cerr << "\n** Error occured while reading \'" << filename
+                  << "\' **\n";
+        return 2;
+    } else {
+        std::cout << "done.\n\n";
+    }
+
+    std::cout << "--Numerical Analysis of " << filename << "--\n";
+    std::cout << "Lowest:  " << low << "\n";
+    std::cout << "Highest: " << high << "\n";
+    std::cout << "Total:   " << total << "\n";
+    std::cout << "Average: " << (total / number_cnt) << "\n\n";
+
     return 0;
 }
