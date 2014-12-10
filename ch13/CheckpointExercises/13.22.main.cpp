4c2283ac40c8fb2a216afba16807b6d67bbbd55e
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-12-10 14:18:41 -0500

    finished ch13 textbook problems

---
+++ ch13/CheckpointExercises/13.22.main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Package
+{
+private:
+    int value;
+public:
+    Package()
+        { value = 7; cout << value << endl; }
+    Package(int v)
+        { value = v; cout << value << endl; }
+    ~Package()
+        { cout << value << endl; }
+};
+
+int main()
+{
+    Package obj1(4);
+    Package obj2(); // compiles as function prototype, not calling constructor
+    Package obj3(2);
+    return 0;
+}
