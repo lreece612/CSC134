62118ec1b4778046048f837ad0a2993250c0537f
lreece612 <123901434+lreece612@users.noreply.github.com>
2023-01-29 15:03:40 -0500

    Update semiprob.cpp

--- ch01/Lab1/semiprob.cpp
+++ ch01/Lab1/semiprob.cpp
@@ -1,23 +1,21 @@
 // This program demonstrates a compile error.
-//
-// Walter Vaughan
+
+// Landon Reece
 
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 int main()
+	
 {
-	int number;
-	float total;
+int number;
+float total;
 	
+cout << "Today is a great day for Lab"
+cout << endl << "Let's start off by typing a number of your choice" << endl;
+cin >> number;
 	
-	cout << "Today is a great day for Lab";
-	cout << endl << "Let's start off by typing a number of your choice" << endl;
-	cin  >> number;
+total = number * 2;
+cout << total << " is twice the number you typed" << endl;
 	
-	total = number * 2;
-	cout << total << " is twice the number you typed" << endl;
-	
-	return 0;	
-}
+return 0;
